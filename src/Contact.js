739cc4cdb5345181cdc91215dfd631979748c1c2
tinalimbu <tinalawati@gmail.com>
2020-10-11 19:50:40 +0100

    Added input boxes to form

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Input } from 'antd';
 
 import './Contact.css';
 
@@ -6,15 +7,23 @@ import './Contact.css';
 class Contact extends React.Component {
 	render() {
     return(
-      <div className="contact">
+      <div className="contact-form">
         <div className="contact-header">
           <p>GET IN TOUCH</p>
         </div>
-        <div>
-          <p>NAME:</p>
-          <p>MOBILE:</p>
-          <p>EMAIL:</p>
-          <p>MESSAGE:</p>
+        <div className="contact-content">
+          <div className="prompts">
+            <p>NAME:</p>
+            <p>MOBILE:</p>
+            <p>EMAIL:</p>
+            <p>MESSAGE:</p>
+          </div>
+          <div className="inputs">
+            <Input className="personal-info" placeholder="Please enter your full name"/>
+            <Input className="personal-info" placeholder="Please enter your modile number"/>
+            <Input className="personal-info" placeholder="Please enter your email address"/>
+            <Input className="message" placeholder="Please enter a message"/>
+          </div>
         </div>
         <div>
           <p>SEND</p>
