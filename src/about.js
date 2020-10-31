28c647467404c4f8a2d2e365165b021fd46bb1d8
tinalimbu <tinalawati@gmail.com>
2020-10-31 21:55:39 +0000

    Added about description

--- src/about.js
+++ src/about.js
@@ -5,13 +5,23 @@ import './about.css';
 
 class About extends React.Component {
 	render() {
+    let placeholderText = `Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+       ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+       culpa qui officia deserunt mollit anim id est laborum.`;
+
     return(
       <div>
         <div className="about-header">
-          <p className="title">FULL NAME</p>
+          <p>FULL NAME</p>
         </div>
         <div>
-          <p>ABOUT</p>
+          <div className="description">
+            <p>{placeholderText}</p>
+          </div>
         </div>
       </div>
     );
