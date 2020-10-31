bb02ba30775b86b1dc180084b68b69969d6c8c7d
tinalimbu <tinalawati@gmail.com>
2020-10-31 21:01:11 +0000

    Added About section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MenuOutlined } from '@ant-design/icons';
 
+import About from './about.js';
 import Menu from './menu.js';
 
 import './App.css';
@@ -31,7 +32,7 @@ class Portfolio extends React.Component {
       content = <Menu/>;
       appBackground = "linear-gradient(135deg, rgba(247,141,229,1) 5%, rgba(223,170,247,1) 50%, rgba(210,159,245,1) 80%)";
     } else {
-      content = "";
+      content = <About/>;
       appBackground = "linear-gradient(135deg, rgba(247,141,229,1) 6%, rgba(223,170,247,1) 41%, rgba(210,159,245,1) 74%)";
     }
 
---
+++ src/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './about.css';
+
+
+class About extends React.Component {
+	render() {
+    return(
+      <div className="about">
+        <div className="about-header">
+          <p className="title">FULL NAME</p>
+        </div>
+        <div>
+          <p>ABOUT</p>
+        </div>
+      </div>
+    );
+	};
+};
+
+export default About;
