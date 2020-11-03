fba39c7bf37c31f64c34d1226e8fe43096d5899b
tinalimbu <tinalawati@gmail.com>
2020-11-03 14:48:57 +0000

    Added full stop

--- src/pages/gateway.js
+++ src/pages/gateway.js
@@ -8,7 +8,7 @@ class Gateway extends React.Component {
 	render() {
     return(
       <div className="gateway">
-        <h2 className="gateway-title">THIS WEBSITE IS PASSWORD PROTECTED</h2>
+        <h2 className="gateway-title">THIS WEBSITE IS PASSWORD PROTECTED.</h2>
         <Input id="password" placeholder="Please enter a valid password to access the content."/>
         <Button className="submit">Submit</Button>
       </div>
