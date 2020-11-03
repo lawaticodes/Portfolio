f0e00c6e338dc5d1aa860882579b8dc98b60fb6e
tinalimbu <tinalawati@gmail.com>
2020-11-03 15:13:20 +0000

    Added password error

--- src/pages/gateway.js
+++ src/pages/gateway.js
@@ -7,6 +7,9 @@ import './gateway.css';
 class Gateway extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      invalidPassword: false,
+    };
     this.submit = this.submit.bind(this);
   };
 
@@ -15,17 +18,27 @@ class Gateway extends React.Component {
     let password = document.getElementById("password").value;
 
     if (validPasswords.includes(password)) {
+      this.setState({invalidPassword: false});
       this.props.unlock();
     } else {
-      let todo;
+      this.setState({invalidPassword: true});
     }
   };
 
 	render() {
+    let invalidPasswordError;
+
+    if (this.state.invalidPassword) {
+      invalidPasswordError = "Invalid password provided.";
+    } else {
+      invalidPasswordError = "";
+    }
+
     return(
       <div className="gateway">
         <h2 className="gateway-title">THIS WEBSITE IS PASSWORD PROTECTED.</h2>
         <Input id="password" placeholder="Please enter a valid password to access the content."/>
+        <p>{invalidPasswordError}</p>
         <Button className="submit" onClick={this.submit}>Submit</Button>
       </div>
     );
