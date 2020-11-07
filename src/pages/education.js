c57ecb6b39ac886bf224f036558b784c245c9397
tinalimbu <tinalawati@gmail.com>
2020-11-07 23:07:43 +0000

    Improve education page

--- src/pages/education.js
+++ src/pages/education.js
@@ -6,47 +6,49 @@ import './education.css';
 class Education extends React.Component {
 	render() {
     return(
-      <div className="education">
+      <div>
         <div className="header">
           <p>FORMAL EDUCATION</p>
         </div>
-        <div className="qualification">
-          <div>
-            <div className="institution">
-              <p>UNDERGRADUATE DEGREE</p>
+        <div className="education">
+          <div className="qualification">
+            <div>
+              <div className="institution">
+                <p>UNDERGRADUATE DEGREE</p>
+              </div>
+              <div className="date">
+                <p>Jan 19 - Dec 19</p>
+              </div>
             </div>
-            <div className="date">
-              <p>Jan 19 - Dec 19</p>
-            </div>
-          </div>
-          <div className="qualifications">
-            <p>BA PHILOSOPHY: 1st</p>
-          </div>
-        </div>
-        <div className="qualification">
-          <div>
-            <div className="institution">
-              <p>A-LEVELS</p>
-            </div>
-            <div className="date">
-              <p>Jan 19 - Dec 19</p>
+            <div className="qualifications">
+              <p>BA PHILOSOPHY: 1st</p>
             </div>
           </div>
-          <div className="qualifications">
-            <p>Maths: A*</p>
-          </div>
-        </div>
-        <div className="qualification gcses">
-          <div>
-            <div className="institution">
-              <p>GCSES</p>
+          <div className="qualification">
+            <div>
+              <div className="institution">
+                <p>A-LEVELS</p>
+              </div>
+              <div className="date">
+                <p>Jan 19 - Dec 19</p>
+              </div>
             </div>
-            <div className="date">
-              <p>Jan 19 - Dec 19</p>
+            <div className="qualifications">
+              <p>Maths: A*</p>
             </div>
           </div>
-          <div className="qualifications">
-            <p>Maths: A*</p>
+          <div className="qualification gcses">
+            <div>
+              <div className="institution">
+                <p>GCSES</p>
+              </div>
+              <div className="date">
+                <p>Jan 19 - Dec 19</p>
+              </div>
+            </div>
+            <div className="qualifications">
+              <p>Maths: A*</p>
+            </div>
           </div>
         </div>
       </div>
