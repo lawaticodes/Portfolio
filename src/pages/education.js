f5bbb15b6848b572ca8da789f0fc823ec6cd8218
tinalimbu <tinalawati@gmail.com>
2020-11-07 23:04:01 +0000

    Improve education page

--- src/pages/education.js
+++ src/pages/education.js
@@ -20,7 +20,33 @@ class Education extends React.Component {
             </div>
           </div>
           <div className="qualifications">
-            <p>BA PHILOSOPHY</p>
+            <p>BA PHILOSOPHY: 1st</p>
+          </div>
+        </div>
+        <div className="qualification">
+          <div>
+            <div className="institution">
+              <p>A-LEVELS</p>
+            </div>
+            <div className="date">
+              <p>Jan 19 - Dec 19</p>
+            </div>
+          </div>
+          <div className="qualifications">
+            <p>Maths: A*</p>
+          </div>
+        </div>
+        <div className="qualification gcses">
+          <div>
+            <div className="institution">
+              <p>GCSES</p>
+            </div>
+            <div className="date">
+              <p>Jan 19 - Dec 19</p>
+            </div>
+          </div>
+          <div className="qualifications">
+            <p>Maths: A*</p>
           </div>
         </div>
       </div>
